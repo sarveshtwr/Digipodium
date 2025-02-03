14fb331a76fef4005dc06f349800cb633920accf
Sarvesh Tiwari <mr.sarveshtiwari@gmail.com>
2025-02-03 17:28:38 +0530

    JS Intro

---
+++ Javascript/condition.js
@@ -0,0 +1,13 @@
+num = 46;
+
+if (num % 2 === 0) {
+  message = "hello";
+  console.log("even");
+  let message2 = "bye";
+  console.log(message2);
+  const pi = 3.14;
+} else {
+  console.log("odd");
+}
+
+console.log(message);
---
+++ Javascript/index.js
@@ -0,0 +1,13 @@
+console.log("Hello Javscript!");
+a = 2332;
+console.log(typeof a);
+console.log(a);
+myname = "Sarvesh";
+console.log(myname);
+console.log(typeof myname);
+console.log(1 + 1);
+
+//exponential operator
+console.log(4 ** 3);
+console.log(1 == "1");
+console.log(1 === "1");
