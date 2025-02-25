fc63d92f4e930b88edfb52d2e94f2be58b3fa164
Sarvesh Tiwari <mr.sarveshtiwari@gmail.com>
2025-02-25 17:29:40 +0530

    formik initialize

--- Section2/frontend/src/app/signup/page.jsx
+++ Section2/frontend/src/app/signup/page.jsx
@@ -1,9 +1,23 @@
+import { useFormik } from "formik";
 import React from "react";
 
 const Signup = () => {
+  const signupForm = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+      //send values to backend
+    },
+  });
+
   return (
     <div>
-      <div className="max-w-md mx-auto mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-neutral-900 dark:border-neutral-700">
+      <div className="max-w-md mx-auto my-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-neutral-900 dark:border-neutral-700">
         <div className="p-4 sm:p-7">
           <div className="text-center">
             <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">
