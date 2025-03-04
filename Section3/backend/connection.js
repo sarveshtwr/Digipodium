cad06f54ee58477be660296f1999b055ac74c5a8
Sarvesh Tiwari <mr.sarveshtiwari@gmail.com>
2025-03-04 18:28:26 +0530

    mongodb

---
+++ Section3/backend/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const url =
+  "mongodb+srv://sarveshtwr:BittuTiwari@cluster0.0hxqz.mongodb.net/mydb?retryWrites=true&w=majority&appName=Cluster0";
+
+// Asynchronous Function - return Promise
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to db");
+  })
+  .catch((err) => {});
+console.log("some other statement");
